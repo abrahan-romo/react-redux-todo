9aec72baf735a287846e43c31675d23104ab5dac
Abraham Ramos <Abraham.Ramos@toshibagcs.com>
2019-06-15 11:29:55 -0500

    second commit

---
+++ src/redux/reducers/todoReducer.js
@@ -0,0 +1,10 @@
+import {UPDATE_VALUE} from "../actions/actionTypes";
+
+const INITIAL_STATE = {
+    value: "",
+    todos: []
+}
+
+export default (state = INITIAL_STATE, action) => {
+    return state;
+}
