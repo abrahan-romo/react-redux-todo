75556f47d80c3ff0e308851dbd3756fa7eae28d4
Abraham Ramos <Abraham.Ramos@toshibagcs.com>
2019-06-15 13:46:32 -0500

    third update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
+const App = () => {
+  return (
+    <React.Fragment>
+      <Typography variant="h2" align="center" gutterBottom>
+        To-Do App
+      </Typography>
 
-const App = () => <h1>Hello World</h1>
+      <Grid container justify="center">
+        <Grid item>
+          <TodoForm />
+        </Grid>
+      </Grid>
 
+      <Grid container justify="center">
+        <Grid item md={8}>
+          <TodoList />
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+};
 
 export default App;
+  
